3b9a4dc369e69fe45333fca4b7c0b8e0e90696b4
fromant <fromant65@gmail.com>
2023-11-07 18:30:25 -0300

    Refactored method tests

--- public_methods/test/methods_test.c
+++ public_methods/test/methods_test.c
@@ -17,7 +17,7 @@ void test_methods()
 
 int comp_char(const void *c1, const void *c2)
 {
-    return strcmp((char *)c1, (char *)c2) == -1;
+    return *(char *)c1 < *(char *)c2;
 }
 
 int comp_int(const void *a, const void *b)
@@ -27,15 +27,15 @@ int comp_int(const void *a, const void *b)
 
 void test_msort()
 {
-
-    char str[8] = "Manuela";
-    void *pstr = &str;
-    char *newstr = (char *)msort(pstr, strlen(str), sizeof(char), comp_char);
-    assert(!strcmp(newstr, "Maaelnu"));
-    int arr[] = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    int *s_arr = msort(arr, 10, sizeof(int), comp_int);
-    for (int i = 0; i < 10; i++)
+    int arr[] = {15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+    int *s_arr = msort(arr, 15, sizeof(int), comp_int);
+    for (int i = 0; i < 15; i++)
     {
         assert(s_arr[i] == i + 1);
     }
+    char str[100] = "murcielago";
+    void *pstr = &str;
+    char *newstr = (char *)msort(pstr, strlen(str), sizeof(char), comp_char);
+    newstr[strlen(str)] = '\0';
+    assert(!strcmp(newstr, "acegilmoru"));
 }
